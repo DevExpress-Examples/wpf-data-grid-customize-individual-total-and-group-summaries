63f9095825875879c51bd58e388bf1948228ef01
DevExpressExampleBot <fakeEmail>
2022-12-01 11:50:39 +0400

    Source auto update [skip ci]

---
+++ VB/DXSample/Data/SampleData.vb
@@ -0,0 +1,18 @@
+Imports DevExpress.Mvvm
+
+Namespace DXSample
+
+    Public Class SampleData
+        Inherits BindableBase
+
+        Public Property City As String
+
+        Public Property Discount As Double
+
+        Public Property Freight As Double
+
+        Public Property Quantity As Integer
+
+        Public Property UnitPrice As Double
+    End Class
+End Namespace
--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -1,22 +1,16 @@
-﻿Imports System
-Imports System.Linq
 Imports System.Windows
-Imports DevExpress.Xpf.Grid
 Imports System.Windows.Controls
-Imports System.Collections.Generic
-Imports System.Windows.Data
-Imports System.Globalization
-Imports System.Windows.Markup
 
 Namespace DXSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
