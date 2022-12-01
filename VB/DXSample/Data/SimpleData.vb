9217f9bbdaa35dc66cfead9c062ecf1c10224f53
DevExpressExampleBot <fakeEmail>
2022-12-01 13:35:45 +0400

    Source auto update [skip ci]

--- VB/DXSample/Data/SimpleData.vb
+++ VB/DXSample/Data/SimpleData.vb
@@ -1,13 +1,18 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 
 Namespace DXSample
+
     Public Class SimpleData
         Inherits BindableBase
 
-        Public Property City() As String
-        Public Property Discount() As Double
-        Public Property Freight() As Double
-        Public Property Quantity() As Double
-        Public Property UnitPrice() As Double
+        Public Property City As String
+
+        Public Property Discount As Double
+
+        Public Property Freight As Double
+
+        Public Property Quantity As Double
+
+        Public Property UnitPrice As Double
     End Class
 End Namespace
